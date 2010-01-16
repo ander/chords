83ca2600692ff009f2d423dd65ba64872441d298
ander <antti.hakala@gmail.com>
2010-01-16 20:49:07 +0200

    first commit

---
+++ lib/note.rb
@@ -0,0 +1,51 @@
+
+module Chords
+  NOTES = ['E', 'F', ['Fs', 'Gb'], 'G', ['Gs', 'Ab'], 'A',
+           ['As', 'Bb'], ['B', 'H'], 'C', ['Cs', 'Db'], 'D', ['Ds', 'Eb']]
+
+  module Note
+    include Comparable
+    attr_reader :interval, :name, :octave
+
+    def self.create(value)
+      octave = value / 12
+      interval = value % 12
+      Chords.const_get([NOTES[interval]].flatten.first).new(octave)
+    end
+
+    def value
+      (@octave * 12) + @interval
+    end
+
+    def <=>(other)
+      if other.respond_to?(:value)
+        value <=> other.value
+      else
+        value <=> other
+      end
+    end
+
+    def coerce(other)
+      if other.respond_to?(:value)
+        [other.value, value]
+      else
+        [other, value]
+      end
+    end
+
+    def +(other); Note.create(value + other) end
+    def -(other); Note.create(value - other) end
+  end
+
+  NOTES.each_with_index do |names, i|
+    names = [names].flatten
+    names.each do |n|
+      eval %Q(class #{n}
+                include Note
+                def initialize(octave=0)
+                  @interval, @octave = #{i}, octave
+                end
+              end)
+    end
+  end
+end
