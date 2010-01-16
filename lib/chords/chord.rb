2cdd925a4ba969af2405392a057abcf70265aa62
ander <antti.hakala@gmail.com>
2010-01-16 21:41:55 +0200

    add some structure

--- lib/chords/chord.rb
+++ lib/chords/chord.rb
@@ -1,9 +1,40 @@
-require 'passant/note'
+require 'chords/note'
+require 'singleton'
 
 module Chords
   
   class Chord
-    
+    def initialize(base, other_notes)
+       
+    end
   end
+  
+  class ChordGenerator
+    include Singleton
+    
+    def create_major(base)
+    end
+    def create_minor(base)
+    end
+    def create_five(base)
+    end
+  end
+  
+  # Extensions to Note so we can say for example 'E.major' to create a chord
+  module ChordExt
+    def major
+      ChordGenerator.instance.create_major(self)
+    end
+    
+    def minor
+      ChordGenerator.instance.create_minor(self)
+    end
+    
+    def five
+      ChordGenerator.instance.create_five(self)
+    end
+  end
+  
+  NOTES.flatten.each{|note_name| Chords.const_get(note_name).extend ChordExt}
   
 end
