67d7b283b2609c0c34e545612f01afadbde7f8cb
ander <antti.hakala@gmail.com>
2010-03-31 10:41:55 +0300

    method renaming

--- lib/chords/pdf_formatter.rb
+++ lib/chords/pdf_formatter.rb
@@ -30,11 +30,11 @@ module Chords
     
     private
     
-    def string_x_dist(box)
+    def string_dist(box)
       box.width / (@fretboard.open_notes.size - 1)
     end
     
-    def string_y_dist(box)
+    def fret_dist(box)
        box.height / (@max_dist + 1)
     end
     
@@ -58,16 +58,15 @@ module Chords
       @pdf.stroke_line box.bottom_left, box.bottom_right
         
       @fretboard.open_notes.each_with_index do |note, note_i|
-        x = box.left + note_i*string_x_dist(box)
+        x = box.left + note_i*string_dist(box)
         @pdf.stroke_line [x, box.top], [x, box.bottom]
-        fretboard_text(note.title, box.left + note_i*string_x_dist(box), box.bottom - 11)
+        fretboard_text(note.title, box.left + note_i*string_dist(box), box.bottom - 11)
       end
         
       @max_dist.times do |n|
-        y = box.top - ((n+1) * string_y_dist(box))
+        y = box.top - ((n+1) * fret_dist(box))
         @pdf.stroke_line [box.left, y], [box.right, y]
-      end 
-      
+      end
     end
     
     def print_fingering(fingering, i)
@@ -75,17 +74,17 @@ module Chords
       
       box = get_box(i)
       
-      rad = ([string_y_dist(box), string_x_dist(box)].min / 2) - 4
+      rad = ([fret_dist(box), string_dist(box)].min / 2) - 4
       
       fingering.each_with_index do |pos, pos_i|
-        fretboard_text((pos || 'x').to_s, box.left + pos_i*string_x_dist(box), box.top + 4)
+        fretboard_text((pos || 'x').to_s, box.left + pos_i*string_dist(box), box.top + 4)
         
         next if [nil, 0].include?(pos)
         
         @pdf.fill_and_stroke do
-          @pdf.circle_at [box.left + (pos_i * (string_x_dist(box))), 
-                          box.top + (string_y_dist(box) / 2) -
-                          (fingering.relative(@max_dist)[pos_i] * string_y_dist(box))], 
+          @pdf.circle_at [box.left + (pos_i * (string_dist(box))), 
+                          box.top + (fret_dist(box) / 2) -
+                          (fingering.relative(@max_dist)[pos_i] * fret_dist(box))], 
                           :radius => rad
         end        
       end
